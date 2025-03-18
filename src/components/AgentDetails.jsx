6acc947c6aad9d9e8a437a300e245544f0567ecf
Grashal Vishwa <89205098+vgrashal16@users.noreply.github.com>
2025-03-18 12:17:00 +0530

    abilities section updated

--- src/components/AgentDetails.jsx
+++ src/components/AgentDetails.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate} from 'react-router-dom';
 
 function AgentDetails(props) {
     const { agentName } = useParams();
     const [agentDetails, setAgentDetails] = useState(null); 
     const agentjson = props.agentjson;
-
+    const navigate = useNavigate();
+    
     useEffect(() => {
         const matchedAgent = agentjson.find(agent => agent.displayName === agentName);
 
@@ -14,14 +15,27 @@ function AgentDetails(props) {
         }
     }, [agentjson, agentName]);
 
+    console.log(agentDetails);
     return (
         <div className="agent-details">
-            <h2>{agentName} Ability Details</h2>
             {agentDetails && (
-                <div>
-                    <p>Agent ID: {agentDetails.uuid}</p>
-                    <p>Agent Description: {agentDetails.description}</p>
+            <> 
+            <div className="agent-potrait">
+                <button className='goBackBtn' onClick={() => navigate('/')}>Go Back</button>
+                <img src={agentDetails.fullPortraitV2}></img>
+            </div>
+            <div className="abilities">
+            {agentDetails.abilities.map((ability, index) => (
+                <div key={index} className="ability">
+                    <img src={ability.displayIcon} alt={ability.displayName} className="ability-icon"/>
+                    <div className="ability-info">
+                        <h3>{ability.displayName}</h3>
+                        <p>{ability.description}</p>
+                    </div>
                 </div>
+            ))}
+            </div>
+            </>  
             )}
         </div>
     );
